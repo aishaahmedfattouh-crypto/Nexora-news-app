eec2914c258d7ec3ee82585d246864d84b55666c
Aisha Ahmed <aishaahmedfattouh@gmail.com>
2026-08-15 23:21:10 +0400

    fix variables

--- Nexora/src/main.jsx
+++ Nexora/src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import "./styles/Variables";
-import "./styles/global";
+import "./styles/variables.css";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
